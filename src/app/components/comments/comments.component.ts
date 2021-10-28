1ea262b868417753e6c33c59953f470105e291ef
Igor Shapilov <v-Igor.Shapilov@lionbridge.com>
2021-10-29 01:48:25 +0800

    Comment form

--- src/app/components/comments/comments.component.ts
+++ src/app/components/comments/comments.component.ts
@@ -2,6 +2,11 @@ import { Component, Input } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Comment } from 'src/app/services/comments.interface';
 
+interface FileObject {
+  target: {
+    files: Array<File>
+  }
+}
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
@@ -11,7 +16,17 @@ export class CommentsComponent {
   @Input()
   comments: Comment[] = [];
 
+  attachment: File | undefined;
+
   onSubmit(form: NgForm): void {
-    console.log(form.value);
+    console.log(form.form.value);
+  }
+
+  fileListener(event: FileObject | Event | null): void {
+    if (!event) {
+      return;
+    }
+    const fileEvent = event as FileObject;
+    this.attachment = fileEvent.target?.files[0];
   }
 }
